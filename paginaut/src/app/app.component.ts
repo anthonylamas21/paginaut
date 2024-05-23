e70bb7cc2684d7d6d622e041f15437b08fc8c846
Anthony <anthony21.lamas@gmail.com>
2024-05-23 11:41:17 -0700

    Agregacion del Navbar

--- paginaut/src/app/app.component.ts
+++ paginaut/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'paginaut';
+}) 
+export class AppComponent implements  AfterViewInit {
+   title = 'paginaut';
+   ngAfterViewInit() {
+    this.initNavbarMenu();
+  }
+
+  initNavbarMenu() {
+    const burger = document.querySelector('.navbar-burger') as HTMLElement;
+    const menu = document.querySelector('.navbar-menu') as HTMLElement;
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      menu.classList.toggle('hidden');
+    });
+  }
 }
