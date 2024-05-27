7907873256e4a959c3607c18fc4e68ce3b877981
Anthony <anthony21.lamas@gmail.com>
2024-05-27 13:00:59 -0700

    Implementacion del navbar con Preline UI
    Se agregaron las librerias y modificaciones necesarias para el funcionamiento del navbar con PrelineUI

--- paginaut/src/app/conocenos/conocenos.component.ts
+++ paginaut/src/app/conocenos/conocenos.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { IStaticMethods } from 'preline/preline';
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 @Component({
   selector: 'app-conocenos',
   templateUrl: './conocenos.component.html',
   styleUrl: './conocenos.component.css'
 })
 export class ConocenosComponent {
+  constructor(private router: Router) {
+    
+  }
 
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
+  }
 }
--- paginaut/tailwind.config.js
+++ paginaut/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: ['./src/**/*.{html,ts}',
+  './node_modules/preline/preline.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
+
+
 
