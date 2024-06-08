c34736de9e734303a9b021e2a22b242e60566d89
Alexis Arath Alatorre Delgadillo <al05-029-0321@utdelacosta.edu.mx>
2024-06-08 12:59:53 -0700

    Archivo actualizado

--- paginaut/src/app/app-routing.module.ts
+++ paginaut/src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
-import { PrincipalComponent } from './principal/principal.component';
-import { BolsadetrabajoComponent } from './bolsadetrabajo/bolsadetrabajo.component';
-import { ConocenosComponent } from './conocenos/conocenos.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: '/principal', pathMatch: 'full' }, // Ruta por defecto
-  { path: 'principal', component: PrincipalComponent }, 
-
-  //Rutas 
-  { path: 'bolsadetrabajo', component: BolsadetrabajoComponent },
-  { path: 'conocenos', component: ConocenosComponent },
-  {path: 'principal', component: PrincipalComponent },
-
-];
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- paginaut/tailwind.config.js
+++ paginaut/tailwind.config.js
@@ -5,18 +5,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      height: {
-        '155': '37rem', // Clase del carousel
-      },
-      fontFamily:{
-        poppins: ['poppins', 'sans-serif']
-      }
+      colors: {
+        'primary-color': '#003C3D',
+        'primary-color-50': '#003C3D',
+        'primary-color-90': '#003C3D',
+        'table-header-color': '#04847C',
+        'color': '#003C3D',
+        'input-bord-color': '#EDEDED',
+        'color-active': '#008779',
+        'input-focus-color': '#000',
+        'secondary-color': '#008779',
+        'secondary-color-90': '#008779e6',
+        'danger-color': '#EF4444',
+    },
+    height: {
+      '155': '37rem', 
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
     },
   },
+},
   plugins: [
     require('preline/plugin'),
   ],
 }
-
-
-
