cbf889b524505d4b1725bd452299a2e594b258d1
Anthony Lamas <anthony21.lamas@gmail.com>
2024-06-12 11:22:13 -0700

    navbar

--- paginaut/src/app/principal/principal.component.ts
+++ paginaut/src/app/principal/principal.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Table,TableModule } from 'primeng/table';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
-export class PrincipalComponent {
+
+export class PrincipalComponent implements OnInit {
+
+  ngOnInit() {
+    // Inicializa Flasher
+    
+  }
+
+
+  scrollToSection(sectionId: string): void {
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
