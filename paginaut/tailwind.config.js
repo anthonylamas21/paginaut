4d2bbd1ff33eb2f0d7fe9cc50b4797316976a950
Alexis Arath Alatorre Delgadillo <al05-029-0321@utdelacosta.edu.mx>
2024-06-16 12:09:52 -0700

    Actualización responsiva del login

--- paginaut/tailwind.config.js
+++ paginaut/tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary-color': '#003C3D',
+        'primary-color-50': '#003C3D',
+        'primary-color-90': '#003C3D',
+        'table-header-color': '#04847C',
+        'color': '#003C3D',
+        'input-bord-color': '#EDEDED',
+        'color-active': '#008779',
+        'input-focus-color': '#000',
+        'secondary-color': '#008779',
+        'secondary-color-90': '#008779e6',
+        'danger-color': '#EF4444',
+    },
       height: {
         '155': '37rem', // Clase del carousel
       },
