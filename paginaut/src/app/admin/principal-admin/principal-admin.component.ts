05dec27a48a1a18a6c86a86a16e532d5fed85783
Saul Hernandez Contreras <saulh3164@gmail.com>
2024-06-25 17:21:56 -0700

    Agregacion admin

---
+++ paginaut/src/app/admin/principal-admin/principal-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-admin',
+  templateUrl: './principal-admin.component.html',
+  styleUrl: './principal-admin.component.css'
+})
+export class PrincipalAdminComponent {
+
+}
