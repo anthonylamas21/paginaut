ab2b34fbe046cec143d68cd300ae4881a09b0c16
Anthony Lamas <anthony21.lamas@gmail.com>
2024-06-27 18:55:15 -0700

    respon

--- paginaut/tailwind.config.js
+++ paginaut/tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   theme: {
     
     extend: {
-      
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       colors: {
         'primary-color': '#003C3D',
         'primary-color-50': '#003C3D',
