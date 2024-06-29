933b359e87b602715735daa56e2742f877d15fdd
Alexis Arath Alatorre Delgadillo <al05-029-0321@utdelacosta.edu.mx>
2024-06-28 18:53:24 -0700

    Formulario de login

--- paginaut/src/app/login/login.component.ts
+++ paginaut/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  FormAccessLogin: FormGroup;
+
+  constructor(public formulario:FormBuilder){
+
+    this.FormAccessLogin = this.formulario.group({
+      email:[''],
+      contrasenia:['']
+    });
+  }
+
+  submitLogin(){
+
+  }
 
 }
