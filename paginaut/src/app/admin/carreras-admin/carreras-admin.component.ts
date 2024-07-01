cc1461020a015a7b7a7e16b52475185edb272b75
José Alberto Peña Ortiz <betitoortiz798@gmail.com>
2024-06-30 18:56:27 -0700

    Creacion del component carrera

---
+++ paginaut/src/app/admin/carreras-admin/carreras-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carreras-admin',
+  templateUrl: './carreras-admin.component.html',
+  styleUrl: './carreras-admin.component.css'
+})
+export class CarrerasAdminComponent {
+
+}
