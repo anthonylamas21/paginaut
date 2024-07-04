6b4b4e1fb85bea3d244b89e0b2e65d882d936db4
Anthony Lamas <anthony21.lamas@gmail.com>
2024-07-03 23:01:00 -0700

    Eliminacion de login

--- paginaut/src/app/login/login.component.ts
+++ paginaut/src/app/login/login.component.ts
@@ -62,29 +62,6 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  FormAccessLogin: FormGroup;
 
-  constructor(public formulario:FormBuilder, public cs: LoginService){
-
-    this.FormAccessLogin = this.formulario.group({
-      email:[''],
-      contrasenia:['']
-    });
-  }
-
-  submitLogin(){
-    //console.log("Hola mundo");
-    const formData = new FormData();
-    formData.append('email', this.FormAccessLogin.get('email')?.value);
-    formData.append('contrasenia', this.FormAccessLogin.get('contrasenia')?.value);
-    console.log('Datos del formulario:', this.FormAccessLogin.value);  
-
-    this.cs.AccessLogin(formData).subscribe(
-    (result) =>{
-      console.log('Resultado:', result);
-    },(error)=>{
-      console.log('Error:', error);
-    });
-  }
 
 }
