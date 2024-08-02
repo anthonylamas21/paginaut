671c2a40afd7c9ab34780b1e953209bf98996a89
José Alberto Peña Ortiz <betitoortiz798@gmail.com>
2024-08-01 22:48:13 -0700

    Cambio en la base de datos

--- api/config/database.php
+++ api/config/database.php
@@ -2,7 +2,7 @@
 class Database
 {
   private $host = "localhost";
-  private $db_name = "UTC2";
+  private $db_name = "UTC";
   private $username = "postgres";
   private $password = "1802";
   public $conn;
