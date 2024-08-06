b5a866f6e4f2448cdca3ffc886065a755ad56e5f
Alexis Arath Alatorre Delgadillo <al05-029-0321@utdelacosta.edu.mx>
2024-08-05 21:10:06 -0700

    beecas, cursos, pscologia, enfermeria modificado

--- api/config/database.php
+++ api/config/database.php
@@ -2,9 +2,9 @@
 class Database
 {
   private $host = "localhost";
-  private $db_name = "UTC";
+  private $db_name = "UTC2";
   private $username = "postgres";
-  private $password = "Blackweb2144";
+  private $password = "Alexis14";
   public $conn;
 
   public function getConnection()
