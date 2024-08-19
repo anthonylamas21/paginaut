17b6cec40f4426e7fca5cb1228ab6848f3476a7b
Alexis Arath Alatorre Delgadillo <alexisaalatorred@gmail.com>
2024-08-19 15:59:50 -0700

    Modales arreglados en piedra en el estanque

--- paginaut/src/app/piedra-estanque/piedra-estanque.component.ts
+++ paginaut/src/app/piedra-estanque/piedra-estanque.component.ts
@@ -41,7 +41,7 @@ export class PiedraEstanqueComponent {
     // más imágenes aquí
   ];
 
-  selectedImage: Image = { url: '', alt: '' };
+  selectedImage: Image | null = null;
 
   openModal(image: Image): void {
     if (image) {
@@ -54,6 +54,14 @@ export class PiedraEstanqueComponent {
     }
   }
 
+  closeModal(): void {
+    const modal = document.getElementById('hs-vertically-centered-modal');
+    if (modal) {
+      modal.classList.add('hidden');
+      modal.classList.remove('pointer-events-auto');
+    }
+    this.selectedImage = null;  // Cambiar de objeto vacío a null
+  }
   
   ngOnInit(): void {
     this.setNavbarColor();
