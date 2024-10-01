5a61e4f883a03ecba370f45288d25edd992d7359
Alexis Arath Alatorre Delgadillo <alexisaalatorred@gmail.com>
2024-09-30 19:32:52 -0700

    Imagenes Complementarias de AGRO terminado

--- api/config/database.php
+++ api/config/database.php
@@ -5,13 +5,14 @@ class Database
   private $db_name = "UTC";
   private $username = "postgres";
   private $password = "Alexis14";
+  private $port = "5433"; // Especifica el puerto aquí
   public $conn;
 
   public function getConnection()
   {
     $this->conn = null;
     try {
-      $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+      $this->conn = new PDO("pgsql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
     } catch (PDOException $exception) {
       echo "Connection error: " . $exception->getMessage();
     }
