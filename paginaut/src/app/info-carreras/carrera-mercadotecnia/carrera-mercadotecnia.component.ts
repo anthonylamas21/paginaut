97702bfff28e4dcde0717f601829d232377a7d45
José Alberto Peña Ortiz <126220456+BetoOrtiz@users.noreply.github.com>
2024-10-04 12:52:30 -0700

    Modificacion en mapa cuatrimestral de las carreras de Administración y Mercadotecnia

--- paginaut/src/app/info-carreras/carrera-mercadotecnia/carrera-mercadotecnia.component.ts
+++ paginaut/src/app/info-carreras/carrera-mercadotecnia/carrera-mercadotecnia.component.ts
@@ -1,7 +1,6 @@
 import { Component, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { Table } from 'primeng/table';
 
-
 interface Item {
   uno: string;
   dos: string;
@@ -13,7 +12,6 @@ interface Item {
   ocho: string;
   nueve: string;
   diez: string;
-  once: string;
 }
 interface Image {
   url: string;
@@ -22,7 +20,7 @@ interface Image {
 @Component({
   selector: 'app-carrera-mercadotecnia',
   templateUrl: './carrera-mercadotecnia.component.html',
-  styleUrl: './carrera-mercadotecnia.component.css'
+  styleUrl: './carrera-mercadotecnia.component.css',
 })
 export class CarreraMercadotecniaComponent {
   currentTab: string = 'segment-1'; // Define el tab inicial
@@ -61,8 +59,7 @@ export class CarreraMercadotecniaComponent {
 
   ngAfterViewInit(): void {
     this.renderer.listen('window', 'load', () => {
-      this.isLoading = false
-
+      this.isLoading = false;
     });
   }
   ngOnInit(): void {
@@ -70,37 +67,37 @@ export class CarreraMercadotecniaComponent {
     this.showTab(this.currentTab); // Mostrar el tab inicial
   }
 
-    // Método para cambiar de pestaña
-    showTab(tabId: string): void {
-      // Oculta todas las pestañas
-      const allTabs = document.querySelectorAll('[role="tabpanel"]');
-      allTabs.forEach(tab => {
-        tab.classList.add('hidden'); // Oculta todas
-      });
+  // Método para cambiar de pestaña
+  showTab(tabId: string): void {
+    // Oculta todas las pestañas
+    const allTabs = document.querySelectorAll('[role="tabpanel"]');
+    allTabs.forEach((tab) => {
+      tab.classList.add('hidden'); // Oculta todas
+    });
 
-      // Muestra la pestaña actual
-      const activeTab = document.getElementById(tabId);
-      if (activeTab) {
-        activeTab.classList.remove('hidden');
-      }
-
-      // Cambiar el tab actual
-      this.currentTab = tabId;
+    // Muestra la pestaña actual
+    const activeTab = document.getElementById(tabId);
+    if (activeTab) {
+      activeTab.classList.remove('hidden');
     }
 
-    // Manejador de clics para los botones de las pestañas
-    onTabClick(event: any, tabId: string): void {
-      this.showTab(tabId);
+    // Cambiar el tab actual
+    this.currentTab = tabId;
+  }
 
-      // Elimina la clase 'active' de todos los botones
-      const allButtons = document.querySelectorAll('[role="tab"]');
-      allButtons.forEach(button => {
-        button.classList.remove('active');
-      });
+  // Manejador de clics para los botones de las pestañas
+  onTabClick(event: any, tabId: string): void {
+    this.showTab(tabId);
 
-      // Agrega la clase 'active' al botón clicado
-      event.target.classList.add('active');
-    }
+    // Elimina la clase 'active' de todos los botones
+    const allButtons = document.querySelectorAll('[role="tab"]');
+    allButtons.forEach((button) => {
+      button.classList.remove('active');
+    });
+
+    // Agrega la clase 'active' al botón clicado
+    event.target.classList.add('active');
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
@@ -133,59 +130,127 @@ export class CarreraMercadotecniaComponent {
   @ViewChild('dt') dt: Table | undefined;
 
   items: Item[] = [
-    {uno: 'Matemáticas', dos: 'Estadística', tres: 'Calidad', cuatro: 'Diseño gráfico', cinco: 'Mercadotecnia internacional', seis: 'Estadia', siete: 'Estadística Aplicada A Los Negocios', ocho: 'Tendencias del mercado y consumidor global', nueve: 'Comunicación Integral De Mercadotecnia', diez: 'Comunicación Ejecutiva', once: 'Estadia'},
+    {
+      uno: 'Inglés I',
+      dos: 'Inglés II',
+      tres: 'Inglés III',
+      cuatro: 'Inglés IV',
+      cinco: 'Inglés V',
+      seis: 'Estadía',
+      siete: 'Inglés VI',
+      ocho: 'Inglés VII',
+      nueve: 'Inglés VIII',
+      diez: 'Estadía',
+    },
+    {
+      uno: 'Desarrollo Humano y Valores',
+      dos: 'Habilidades Socioemocionales y Manejo de Conflictos',
+      tres: 'Desarrollo del Pensamiento y Toma de Decisiones',
+      cuatro: 'Ética Profesional',
+      cinco: 'Liderazgo de Equipos de Alto Desempeño',
+      seis: 'Estadía',
+      siete: 'Habilidades Gerenciales',
+      ocho: 'Mercadotecnia Digital II',
+      nueve: 'Cultura Emprendedora',
+      diez: 'Estadía',
+    },
+    {
+      uno: 'Mercadotecnia',
+      dos: 'Estadística I',
+      tres: 'Legislación Comercial',
+      cuatro: 'Mezcla Promocional',
+      cinco: 'Logística y Distribución',
+      seis: 'Estadía',
+      siete: 'Estadística Aplicada a los Negocios',
+      ocho: 'Inteligencia de Mercados',
+      nueve: 'Cadena de Suministro',
+      diez: 'Estadía',
+    },
+    {
+      uno: 'Matemáticas',
+      dos: 'Planeación Estratégica',
+      tres: 'Estadística II',
+      cuatro: 'Diseño Digital y Multimedia',
+      cinco: 'Mercadotecnia de Servicios',
+      seis: 'Estadía',
+      siete: 'Mercadotecnia Internacional',
+      ocho: 'Gestión de la Calidad',
+      nueve: 'Plan de Negocios',
+      diez: 'Estadía',
+    },
+    {
+      uno: 'Informática',
+      dos: 'Contabilidad para Negocios',
+      tres: 'Sistema de Investigación de Mercados I',
+      cuatro: 'Sistema de Investigación de Mercados II',
+      cinco: 'Mercadotecnia Digital I',
+      seis: 'Estadía',
+      siete: 'Desarrollo de Nuevos Productos',
+      ocho: 'Inteligencia Financiera',
+      nueve: 'Comunicación Integral de la Mercadotecnia',
+      diez: 'Estadía',
+    },
+    {
+      uno: 'Fundamentos de Administración y Entorno Empresarial',
+      dos: 'Comportamiento del Consumidor',
+      tres: 'Estrategias de Producto y Precio',
+      cuatro: 'Gestión de Ventas',
+      cinco: 'Mercadotecnia Estratégica',
+      seis: 'Estadía',
+      siete: 'Tendencias del Mercado y Consumidor Global',
+      ocho: 'Administración de la Producción',
+      nueve: 'Derecho Corporativo',
+      diez: 'Estadía',
+    },
+    {
+      uno: 'Comunicación y Habilidades Digitales',
+      dos: 'Economía',
+      tres: 'Proyecto Integrador I',
+      cuatro: 'Administración del Tiempo',
+      cinco: 'Proyecto Integrador II',
+      seis: 'Estadía',
+      siete: 'Planeación y Organización del Trabajo',
+      ocho: 'Gestión del Talento Humano',
+      nueve: 'Proyecto Integrador III',
+      diez: 'Estadía',
+    },
+  ];
 
-    {uno: 'Administración', dos: 'Informática II', tres: 'Gestión de proyectos', cuatro: 'Logística y distribución', cinco: 'Mezcla promocional II', seis: 'Estadia', siete: 'Desarrollo De Nuevos Productos', ocho: 'Administración De La Producción', nueve: 'Planeación Y Seguimiento De Proyectos', diez: 'Cadena De Suministros', once: 'Estadia'},
-
-    {uno: 'Informática I', dos: 'Contabilidad', tres: 'Legislación comercial', cuatro: 'Comportamiento del consumidor', cinco: 'Mercadotecnia digital', seis: 'Estadia', siete: 'Inteligencia De Mercados', ocho: 'Gestión Del Talento Humano', nueve: 'Finanzas', diez: 'Plan De Negocios', once: 'Estadia'},
-
-    {uno: 'Mercadotecnia', dos: 'Planeación estratégica', tres: 'Investigación de mercados II', cuatro: 'Estrategias de precios', cinco: 'Mercadotecnia estratégica', seis: 'Estadia', siete: 'Inglés VI', ocho: 'Optativa I', nueve: 'Optativa II', diez: 'Integradora', once: 'Estadia'},
-
-    {uno: 'Economía', dos: 'Investigación de mercados I', tres: 'Integradora I', cuatro: 'Mezcal promocional I', cinco: 'Integradora II', seis: 'Estadia', siete: 'Administración Del Tiempo', ocho: 'Inglés VII', nueve: 'Inglés VIII', diez: 'Inglés IX', once: 'Estadia'},
-
-    {uno: 'Inglés I', dos: 'Ventas', tres: 'Estrategias de productos', cuatro: 'Metodología de la investigación', cinco: 'Inglés V', seis: 'Estadia', siete: '', ocho: 'Planeación y organización del trabajo', nueve: 'Dirección De Equipos De Alto Rendimiento', diez: 'Negociación Empresarial', once: 'Estadia'},
-
-    {uno: 'Expresión oral y escrita I', dos: 'Inglés II', tres: 'Inglés III', cuatro: 'Inglés IV', cinco: 'Expresión oral y escrita II', seis: 'Estadia', siete: '', ocho: '', nueve: '', diez: '', once: 'Estadia'},
-
-    {uno: 'Formación sociocultural I', dos: 'Formación sociocultural II', tres: 'Formación sociocultural III', cuatro: 'Formación sociocultural IV', cinco: '', seis: 'Estadia', siete: '', ocho: '', nueve: '', diez: '', once: 'Estadia'}
-]
-;
-
-//INICIO TABLA CUATRIMESTRES
-onMouseOver(columna: string, valor: any) {
-  if(valor == ""){
-    //console.log(`El elemento de la columna ${columna} con valor "NULO" fue seleccionado`);
-  }else{
-    //console.log(`El elemento de la columna ${columna} con valor "${valor}" fue seleccionado`);
+  //INICIO TABLA CUATRIMESTRES
+  onMouseOver(columna: string, valor: any) {
+    if (valor == '') {
+      //console.log(`El elemento de la columna ${columna} con valor "NULO" fue seleccionado`);
+    } else {
+      //console.log(`El elemento de la columna ${columna} con valor "${valor}" fue seleccionado`);
+    }
   }
-}
 
-openModal(image: Image): void {
-  this.selectedImage = image;
-  const modal = document.getElementById('hs-vertically-centered-modal');
-  if (modal) {
-    modal.classList.remove('hidden');
-    modal.classList.add('pointer-events-auto');
+  openModal(image: Image): void {
+    this.selectedImage = image;
+    const modal = document.getElementById('hs-vertically-centered-modal');
+    if (modal) {
+      modal.classList.remove('hidden');
+      modal.classList.add('pointer-events-auto');
+    }
   }
-}
-closeModal(): void {
-  const modal = document.getElementById('hs-vertically-centered-modal');
-  if (modal) {
-    modal.classList.add('hidden');
-    modal.classList.remove('pointer-events-auto');
+  closeModal(): void {
+    const modal = document.getElementById('hs-vertically-centered-modal');
+    if (modal) {
+      modal.classList.add('hidden');
+      modal.classList.remove('pointer-events-auto');
+    }
+    this.selectedImage = { url: '', alt: '' };
   }
-  this.selectedImage = { url: '', alt: '' };
-}
 
-ampliarImagen(imagenUrl: string): void {
-  this.imagenAmpliada = imagenUrl;
-}
+  ampliarImagen(imagenUrl: string): void {
+    this.imagenAmpliada = imagenUrl;
+  }
 
-cerrarImagenAmpliada(): void {
-  this.imagenAmpliada = null;
-}
+  cerrarImagenAmpliada(): void {
+    this.imagenAmpliada = null;
+  }
 
-//FIN TABLA CUATRIMESTRES
+  //FIN TABLA CUATRIMESTRES
 
   filterGlobal(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -194,60 +259,64 @@ cerrarImagenAmpliada(): void {
     }
   }
 
-
   mostrar(elemento: any): void {
     // Verifica si el elemento recibido es un botón
     if (elemento.tagName.toLowerCase() === 'button') {
-        const tooltipElement = elemento.querySelector('.hs-tooltip');
-        if (tooltipElement) {
-            tooltipElement.classList.toggle('show');
-            const tooltipContent = tooltipElement.querySelector('.hs-tooltip-content');
-            if (tooltipContent) {
-                tooltipContent.classList.toggle('hidden');
-                tooltipContent.classList.toggle('invisible');
-                tooltipContent.classList.toggle('visible');
-                // Ajustar la posición del tooltip
-                TooltipManager.adjustTooltipPosition(elemento, tooltipContent);
-            }
+      const tooltipElement = elemento.querySelector('.hs-tooltip');
+      if (tooltipElement) {
+        tooltipElement.classList.toggle('show');
+        const tooltipContent = tooltipElement.querySelector(
+          '.hs-tooltip-content'
+        );
+        if (tooltipContent) {
+          tooltipContent.classList.toggle('hidden');
+          tooltipContent.classList.toggle('invisible');
+          tooltipContent.classList.toggle('visible');
+          // Ajustar la posición del tooltip
+          TooltipManager.adjustTooltipPosition(elemento, tooltipContent);
         }
+      }
     }
   }
-
 }
 
 class TooltipManager {
-  static adjustTooltipPosition(button: HTMLElement, tooltip: HTMLElement): void {
-      // Obtener dimensiones del botón y del tooltip
-      const buttonRect = button.getBoundingClientRect();
-      const tooltipRect = tooltip.getBoundingClientRect();
+  static adjustTooltipPosition(
+    button: HTMLElement,
+    tooltip: HTMLElement
+  ): void {
+    // Obtener dimensiones del botón y del tooltip
+    const buttonRect = button.getBoundingClientRect();
+    const tooltipRect = tooltip.getBoundingClientRect();
 
-      // Obtener dimensiones de la ventana
-      const windowWidth = window.innerWidth;
-      const windowHeight = window.innerHeight;
+    // Obtener dimensiones de la ventana
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
 
-      // Calcular la posición preferida del tooltip
-      const preferredLeft = buttonRect.left - tooltipRect.width / 2 + buttonRect.width / 2;
-      const preferredTop = buttonRect.top - tooltipRect.height - 10; // Espacio entre el botón y el tooltip
+    // Calcular la posición preferida del tooltip
+    const preferredLeft =
+      buttonRect.left - tooltipRect.width / 2 + buttonRect.width / 2;
+    const preferredTop = buttonRect.top - tooltipRect.height - 10; // Espacio entre el botón y el tooltip
 
-      // Ajustar la posición si se sale de la pantalla hacia la izquierda
-      let left = Math.max(preferredLeft, 0);
+    // Ajustar la posición si se sale de la pantalla hacia la izquierda
+    let left = Math.max(preferredLeft, 0);
 
-      // Ajustar la posición si se sale de la pantalla hacia arriba
-      let top = Math.max(preferredTop, 0);
+    // Ajustar la posición si se sale de la pantalla hacia arriba
+    let top = Math.max(preferredTop, 0);
 
-      // Ajustar la posición si el tooltip se sale de la pantalla hacia la derecha
-      if (left + tooltipRect.width > windowWidth) {
-          left = windowWidth - tooltipRect.width;
-      }
+    // Ajustar la posición si el tooltip se sale de la pantalla hacia la derecha
+    if (left + tooltipRect.width > windowWidth) {
+      left = windowWidth - tooltipRect.width;
+    }
 
-      // Ajustar la posición si el tooltip se sale de la pantalla hacia abajo
-      if (top + tooltipRect.height > windowHeight) {
-          top = windowHeight - tooltipRect.height;
-      }
+    // Ajustar la posición si el tooltip se sale de la pantalla hacia abajo
+    if (top + tooltipRect.height > windowHeight) {
+      top = windowHeight - tooltipRect.height;
+    }
 
-      // Aplicar posición al tooltip
-      tooltip.style.position = 'fixed';
-      tooltip.style.top = `${top}px`;
-      tooltip.style.left = `${left}px`;
+    // Aplicar posición al tooltip
+    tooltip.style.position = 'fixed';
+    tooltip.style.top = `${top}px`;
+    tooltip.style.left = `${left}px`;
   }
 }
